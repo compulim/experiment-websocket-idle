94edd40dacbcd6dcc0e50de59b56b78e5e085b0c
William Wong <compulim@hotmail.com>
2022-10-23 02:04:54 -0700

    Print close and timestamp

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import { resolve } from 'path';
 import { WebSocketServer } from 'ws';
 import createExpress, { static as createStaticMiddleware } from 'express';
 
+let lastLogTime = Date.now();
+
+function log(message) {
+  const now = new Date();
+
+  console.log(`${now.toLocaleTimeString()} (+${+now - lastLogTime}ms): ${message}`);
+  lastLogTime = now;
+}
+
 (function () {
   const app = createExpress();
   const { PORT = 5000 } = process.env;
@@ -10,26 +19,30 @@ import createExpress, { static as createStaticMiddleware } from 'express';
   app.get('/health.txt', (_, res) => res.send('OK'));
   app.get('/', createStaticMiddleware(resolve(__dirname, '../public/')));
 
-  console.log(resolve(__dirname, '../public/'));
+  log(resolve(__dirname, '../public/'));
 
   const server = createServer(app);
   const webSocketServer = new WebSocketServer({ server });
 
   webSocketServer.on('connection', ws => {
-    console.log(`Connected`);
+    log(`Connected`);
 
     ws.send('Hello, World!');
 
     ws.on('message', data => {
       const text = data.toString('utf8');
 
-      console.log(text);
+      log(text);
 
       ws.send(text);
+    });
+
+    ws.on('close', data => {
+      log('Closed');
     });
   });
 
   server.listen(PORT, () => {
-    console.log(`Listening to port ${PORT}`);
+    log(`Listening to port ${PORT}`);
   });
 })();
