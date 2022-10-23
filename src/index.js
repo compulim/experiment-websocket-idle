513fdd3193100e99007e6d58379fa2f7ca9a43f0
William Wong <compulim@hotmail.com>
2022-10-22 18:43:49 -0700

    Add HTML files and Web Socket

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import createExpress from 'express';
 import { createServer } from 'http';
+import { fileURLToPath } from 'url';
+import { resolve } from 'path';
+import { WebSocketServer } from 'ws';
+import createExpress, { static as createStaticMiddleware } from 'express';
 
-const app = createExpress();
-const { PORT = 5000 } = process.env;
+(function () {
+  const app = createExpress();
+  const { PORT = 5000 } = process.env;
 
-app.get('/health.txt', (_, res) => res.send('OK'));
+  app.get('/health.txt', (_, res) => res.send('OK'));
+  app.get('/', createStaticMiddleware(resolve(fileURLToPath(import.meta.url), '../../public/')));
 
-createServer(app).listen(PORT, () => {
-  console.log(`Listening to port ${PORT}`);
-});
+  console.log(resolve(fileURLToPath(import.meta.url), '../../public/'));
+
+  const server = createServer(app);
+  const webSocketServer = new WebSocketServer({ server });
+
+  webSocketServer.on('connection', ws => {
+    console.log(`Connected`);
+
+    ws.send('Hello, World!');
+  });
+
+  server.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`);
+  });
+})();
